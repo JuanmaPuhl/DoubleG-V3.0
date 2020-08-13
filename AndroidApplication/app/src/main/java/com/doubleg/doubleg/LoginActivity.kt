53620dbc48f855455f41958a96db790f09b1f30b
JuanmaPuhl <puhljm@gmail.com>
2020-08-13 01:36:57 -0300

    mmm no me gusta asi

--- AndroidApplication/app/src/main/java/com/doubleg/doubleg/LoginActivity.kt
+++ AndroidApplication/app/src/main/java/com/doubleg/doubleg/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.doubleg.doubleg
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_login)
     }
 }
